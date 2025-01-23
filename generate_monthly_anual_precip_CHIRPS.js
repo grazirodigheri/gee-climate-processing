30244050e0125a9c6b53b62a38a865348bb5ff0f
grazirodigheri <grazielirodigheri@gmail.com>
2025-01-23 14:18:35 -0300

    :zap: process the image collection to generate mean image

--- generate_monthly_anual_precip_CHIRPS.js
+++ generate_monthly_anual_precip_CHIRPS.js
@@ -16,9 +16,9 @@ var climateColl = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
 // Define the scale of the collection
 var collScale = 5566
  
-// Define the start and end of period
-var startDate = '1991-01-01';
-var endDate = '2020-12-31';
+// Define the start and end of the period
+var startYear = '1991';
+var endYear = '2020';
 
 // =========================================================
 // ====== Function to compute monthly precipitation  =======
@@ -32,90 +32,124 @@ var endDate = '2020-12-31';
 * @param endDate (Date): End date of the period  (format: "AAAA-MM-DD")
 */
 
-// Select and filter the collection
+// Function to calculate monthly averages by year and consolidate into a single collection
 var computeSumMonthly = function(coll, ROI, startDate, endDate) {
+  // Filter collection
   var collection = coll
     .filterBounds(ROI)
     .filterDate(startDate, endDate)
     .select('precipitation');
 
-  print("Collection", collection.limit(100));
-  Map.addLayer(collection.first().clip(ROI), 
-    {min: 1, max: 17, palette: ['001137', '0aab1e', 'e7eb05', 'ff4a2d', 'e90000']}, 
-    'Precipitation');
-  
-  // Add 'month' and 'year' properties to each image
-  var collectionWithMonthYear = collection.map(function(image) {
-    var date = ee.Date(image.get('system:time_start'));
-    var month = date.get('month'); // Get the month
-    var year = date.get('year');   // Get the year
-    return image.set('month', month).set('year', year);
-  });
+  // List of unique years and months based on `system:time_start`
+  var years = collection.aggregate_array('system:time_start')
+    .map(function(ts) {
+      return ee.Date(ts).get('year');
+    }).distinct();
+  var months = ee.List.sequence(1, 12); // Months from 1 to 12
 
-  // Get the list of unique years in the collection
-  var years = collectionWithMonthYear.aggregate_array('year').distinct();
+  // Generate monthly averages by year
+  var monthlySumByYear = years.map(function(year) {
+    return months.map(function(month) {
+      var filtered = collection
+        .filter(ee.Filter.calendarRange(year, year, 'year'))
+        .filter(ee.Filter.calendarRange(month, month, 'month'));
 
-  // Calculate the accumulated precipitation for each month and year
-  var anualSum = years.map(function(year) {
-    var mensalSum = ee.List.sequence(1, 12).map(
-      function(month) {
-        var monthCollection = collectionWithMonthYear
-          .filter(ee.Filter.eq('month', month))
-          .filter(ee.Filter.eq('year', year))
-        return ee.Algorithms.If(
-          monthCollection.size().eq(0), 
-          null, 
-          monthCollection.sum().set('month', month)
-        )
-      },
-      true
-    )
-    return mensalSum
-  })
-  anualSum = ee.ImageCollection(anualSum.flatten())
-  print(anualSum)
-  
-  // Calculate the mean precipitation for each month
-  var mean_mensal = ee.List.sequence(1, 12).map(function(month) {
-    var monthlyMean = anualSum
-      .filter(ee.Filter.eq('month', month))
-      .mean()
-      .reduceRegion({
-        reducer: ee.Reducer.mean(),
-        geometry: ROI,
-        scale: collScale,
-        maxPixels: 1e13
-      })
-      .get('precipitation');
-
-    return ee.Feature(null, {
-      'month': month,
-      'mean_precipitation': monthlyMean
+      // Calculate the sum for the specific month and year
+      var meanImage = filtered.sum().set({'year': year, 'month': month});
+      return meanImage;
     });
-  })
-  mean_mensal = ee.FeatureCollection(mean_mensal)
-  print(mean_mensal)
+  }).flatten();
+  // Transform into an ImageCollection
+  var monthlyCollection = ee.ImageCollection(monthlySumByYear);
 
-  return mean_mensal;
+  // Calculate the annual monthly average (average for each month across all years)
+  var monthlyAnnualMeans = months.map(function(month) {
+    var filtered = monthlyCollection.filter(ee.Filter.eq('month', month));
+    var meanImage = filtered.mean().rename("mean");
+    var stdDevImage = filtered.reduce(ee.Reducer.stdDev()).rename("std");
+    return meanImage.addBands(stdDevImage).set({'month': month});
+  });
+  // Transform into an ImageCollection
+  var monthlyAnnualCollection = ee.ImageCollection(monthlyAnnualMeans);
+
+  // Calculate the mean and standard deviation for all 12 images
+  var meanImage = monthlyAnnualCollection.select("mean").mean().clip(ROI);
+  var stdDevImage = monthlyAnnualCollection.select("mean").reduce(ee.Reducer.stdDev()).clip(ROI);
+
+  return {
+    monthlyCollection: monthlyCollection,
+    monthlyAnnualCollection: monthlyAnnualCollection,
+    meanImage: meanImage,
+    stdDevImage: stdDevImage
+  };
 };
 
 // ====================================================================================
 // Pre-process the ROI
-var geomRoi = roi.geometry()
-var line_geom = ee.Image().paint(roi,'vazio', 1).eq(0)
+var geomRoi = roi.geometry();
+var line_geom = ee.Image().paint(roi,'vazio', 1).eq(0);
 var visPar = {'palette':'000000','opacity': 1};
-Map.addLayer(line_geom, visPar, 'Region Shape')
 
 // Compute the monthly precipitation
-var monthlySumPrecipitacion = computeSumMonthly(climateColl, geomRoi, startDate, endDate);
-print("Monthly Precipitacion", monthlySumPrecipitacion)
+var result = computeSumMonthly(climateColl, geomRoi, startYear+'-01-01', endYear+'-12-31');
 
-// Export the result
+// Visualize the collections
+Map.addLayer(result.stdDevImage, 
+  {min: 10, max: 45, palette: ['#fcffa4', '#fca50a', '#dd513a', '#932667', '#420a68', '#000004']}, "StdDev");
+Map.addLayer(result.meanImage, 
+  {min: 50, max: 180, palette: ['#fde725', '#7ad151', '#22a884', '#2a788e', '#414487', '#440154']}, "Mean");
+Map.addLayer(line_geom, visPar, 'Region Shape');
+
+// Export the mean image of all months to Google Drive
+var exportImg = function(img, name) {
+  Export.image.toDrive({
+  image: img, 
+  description: name, 
+  folder: 'GEE_exports', 
+  fileNamePrefix: name, 
+  region: geomRoi,  
+  scale: collScale, 
+  maxPixels: 1e8, 
+  crs: 'EPSG:4326' 
+});
+}
+exportImg(result.meanImage, 'mean_precipitation_all_months')
+exportImg(result.stdDevImage, 'std_precipitation_all_months')
+
+// ====================================================================================
+// Export CSV
+
+// Regional Reduction for Annual Monthly Averages
+var monthlyStats = result.monthlyAnnualCollection.map(function(image) {
+  var meanValue = image.reduceRegion({
+    reducer: ee.Reducer.mean(),
+    geometry: geomRoi,
+    scale: collScale,
+    maxPixels: 1e13
+  }).get('mean');
+
+  var stdValue = image.reduceRegion({
+    reducer: ee.Reducer.mean(),
+    geometry: geomRoi,
+    scale: collScale,
+    maxPixels: 1e13
+  }).get('std');
+
+  return ee.Feature(null, {
+    'month': image.get('month'),
+    'mean': meanValue,
+    'std': stdValue
+  });
+});
+// Transform into FeatureCollection
+var monthlyFeatureCollection = ee.FeatureCollection(monthlyStats);
+
+// Export the collections or processed data
 Export.table.toDrive({
-  collection: monthlySumPrecipitacion,
-  description: 'precip_CHIRPS_'+startDate+"_"+endDate,
+  collection: monthlyFeatureCollection,
+  description: 'precip_CHIRPS_'+startYear+"_"+endYear,
   folder: "GEE_tables",
-  fileNamePrefix: 'precip_CHIRPS_'+startDate+"_"+endDate,
+  fileNamePrefix: 'precip_CHIRPS_'+startYear+"_"+endYear,
   fileFormat: 'CSV'
 });
 
@@ -132,9 +166,9 @@ var monthNames = [
 
 // Precipitation chart
 var precipitationChart = ui.Chart.feature.byFeature({
-  features: monthlySumPrecipitacion, 
+  features: monthlyFeatureCollection, 
   xProperty: 'month',
-  yProperties: ['mean_precipitation']
+  yProperties: ['mean']
 })
   .setChartType('ColumnChart')
   .setOptions({
